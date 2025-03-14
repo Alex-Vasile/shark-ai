29328d16a3d4b1efa670c218e89e14f24bc36d1e
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-03-14 12:37:12 -0700

    pre-commit pipeliened_kv_cache_test

--- sharktank/tests/layers/pipelined_sharded_paged_kv_cache_test.py
+++ sharktank/tests/layers/pipelined_sharded_paged_kv_cache_test.py
@@ -37,7 +37,9 @@ class PipelinedShardedPagedKVCacheTest(unittest.TestCase):
         for block in range(self.transformer_block_count):
             pp_group = int(block * self.pipeline_count / self.transformer_block_count)
             zero_4_group = self.shard_count * pp_group
-            block_to_device_lookup.append(tuple(i + zero_4_group for i in range(self.shard_count)))
+            block_to_device_lookup.append(
+                tuple(i + zero_4_group for i in range(self.shard_count))
+            )
         self.block_to_device_lookup = tuple(block_to_device_lookup)
 
         self.cache = PagedKVCache(
