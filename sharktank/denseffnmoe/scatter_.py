5e5741498f2677f6b9b2f46b60127aaab4226b57
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-06-11 16:26:05 +0000

    update scatter_ script
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/denseffnmoe/scatter_.py
+++ sharktank/denseffnmoe/scatter_.py
@@ -18,36 +18,26 @@ class ScatterLayer(ThetaLayer):
     def generate_random_theta(self):
         return
 
-    def forward(
-        self,
-        h: torch.Tensor,
-        top_experts_index: torch.Tensor,
-        expert_gate: torch.Tensor,
-    ) -> torch.Tensor:
+    def forward(self, attn_output: torch.Tensor) -> torch.Tensor:
         """
         h: (batch_size * sequence_length, input_feature_dim)
         top_experts_index: (batch_size * sequence_length, num_top_experts)
         expert_gate: (batch_size * sequence_length, num_top_experts)
         """
-        num_experts = 4
-        num_tokens, input_feature_dim = h.shape
+        self.v_head_dim = 128
 
-        # (self.num_experts, num_tokens)
-        router_indices = torch.arange(num_tokens).view(1, -1).expand(num_experts, -1)
-        # (self.num_experts * num_tokens, input_feature_dim)
-        router_indices = (
-            router_indices.reshape(-1, 1)
-            .expand(-1, input_feature_dim)
-            .to(dtype=torch.float)
-        )
-        return router_indices.view(num_experts, num_tokens, input_feature_dim).sum(
-            dim=0
-        )
+        # attn_output = attn_output[:, :, :, : self.v_head_dim]
+
+        # attn_output = attn_output.transpose(1, 2)
+
+        # attn_output = attn_output.flatten(2)
+
+        return ops.linear(attn_output, attn_output[..., : attn_output.shape[-1]])
 
 
 import os
 
-name = "fail"
+name = "isolated"
 
 cwd = os.path.realpath(os.curdir)
 cwd = "/home/alvasile/repos/shark-ai/sharktank/denseffnmoe/"
@@ -57,29 +47,21 @@ iree_result_path = cwd + "iree_result.npy"
 
 model = ScatterLayer(Theta([DefaultPrimitiveTensor(data=torch.tensor([1]))]))
 
-ffn_input_path = cwd + "ffn_input.npy"
-expert_gate_path = cwd + "expert_gate.npy"
-top_experts_index_path = cwd + "top_k_experts.npy"
-ffn_input = torch.tensor(np.load(ffn_input_path))
-expert_gate = torch.tensor(np.load(expert_gate_path))
-top_k_experts = torch.tensor(np.load(top_experts_index_path))
-expected_output = torch.tensor(np.load(cwd + "eager_moe_output.npy"))
+attn_output_path = cwd + "attn_output.npy"
+# b_path = cwd + "b.npy"
+attn_output = torch.tensor(np.load(attn_output_path))
 
 # Run locally
-eager_result = model.forward(ffn_input, top_k_experts, expert_gate)
+eager_result = model.forward(attn_output)
 # assert torch.isclose(
 # eager_result, expected_output, rtol=1.3e-6, atol=1e-5
 # ).all(), "Eager result does not match expected output"
 
 
-num_tokens = torch.export.Dim("num_tokens")
-input_feature_dim = torch.export.Dim("input_feature_dim")
-num_top_experts = torch.export.Dim("num_top_experts")
+dynamic = torch.export.Dim("dynamic")
 
 dynamic_shapes = {
-    "ffn_input": {0: num_tokens},  # , 1: input_feature_dim},
-    "top_k_experts": {},  # {0: num_tokens},  # , 1: num_top_experts},
-    "expert_gate": {},  # {0: num_tokens},  # , 1: num_top_experts},
+    "attn_output": {1: dynamic},  # , 1: input_feature_dim},
 }
 
 # Run through IREE
@@ -88,12 +70,12 @@ fxb = FxProgramsBuilder(model)
 
 @fxb.export_program(
     name="scatter",
-    args=(ffn_input, top_k_experts, expert_gate),
-    dynamic_shapes=dynamic_shapes,
+    args=(attn_output,),
+    # dynamic_shapes=dynamic_shapes,
     strict=False,
 )
-def _(model, ffn_input, top_k_experts, expert_gate) -> torch.Tensor:
-    return model(ffn_input, top_k_experts, expert_gate)
+def _(model, attn_output) -> torch.Tensor:
+    return model(attn_output)
 
 
 output = export(fxb, import_symbolic_shape_expressions=True)
@@ -124,9 +106,7 @@ run_args = [
     f"--module={iree_module_path}",
     "--device=hip://0",
     "--function=scatter",
-    f"--input=@{ffn_input_path}",
-    f"--input=@{top_experts_index_path}",
-    f"--input=@{expert_gate_path}",
+    f"--input=@{attn_output_path}",
     f"--output=@{iree_result_path}",
 ]
 cmd = subprocess.list2cmdline(run_args)
